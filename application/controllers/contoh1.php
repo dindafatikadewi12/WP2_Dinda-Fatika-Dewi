dc6e91b9b244b227611135df991f89f612f9770a
dindafatikadewi12 <dindafatikadewi@gmail.com>
2022-09-18 17:18:20 +0700

    upload awal project

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+public function index()
+{
+    echo"<h1>perkenalkan</h1>"; 
+    echo"nama saya dinda fatika dewi
+        saya mahasiswi ubsi pontianak jurusan sistem informasi";
+    }
+}
+?>
