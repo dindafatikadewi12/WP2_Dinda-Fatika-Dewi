364be6707d60aeb69ceb8af6d71d9e21f74c2e99
dindafatikadewi12 <dindafatikadewi@gmail.com>
2022-10-03 17:07:34 +0700

    tugas pertemuan 3

--- application/controllers/web.php
+++ application/controllers/web.php
@@ -14,4 +14,11 @@ class Web extends CI_Controller{
         $this->load->view('v_index',$data);
         $this->load->view('v_footer',$data);
     }
+    public function about()
+    {
+        $data['judul']= "Halaman about";
+        $this->load->view('v_header',$data);
+        $this->load->view('V_about',$data);
+        $this->load->view('v_footer',$data);
+    }
 }
---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<selection>
+    <h1><php echo $judul ?></h1>
+    <h4>nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dinda</li>
+        <li>Nama Belakang : Fatika Dewi</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> asrama gatot subroto 1 no c.40</li>
+</ul>
+
+<h4>Tempat lahir</h4>
+<ul type="none">
+    <li>cirebon</li>
+</ul>
+
+<h4>olahraga favorit</h4>
+<ul type="square">
+    <li>Basket</li>
+    <li>memanah</li>
+</ul>
+</selection>
+
